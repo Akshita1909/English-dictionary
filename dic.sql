0ac0b839a287450846e12a73a638c3df16007e77
Akshita1909 <116419755+Akshita1909@users.noreply.github.com>
2024-04-28 13:03:36 +0530

    Add files via upload

---
+++ dic.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE dictionary;
+USE dictionary;
+
+CREATE TABLE Users (
+    First_Name VARCHAR(30) NOT NULL,
+    Last_Name VARCHAR(30) NOT NULL,
+    Contact_Info VARCHAR(50) NOT NULL,
+    User_Password VARCHAR(64) NOT NULL, 
+    DOB DATE
+);
+
+SELECT * FROM Users;
+
+
+ALTER USER 'root'@'localhost' identified with mysql_native_password by '@Abhi2003';
