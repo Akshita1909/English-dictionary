731223e3cff05e68b51f72cdebfea82fd9d93c4d
Akshita1909 <116419755+Akshita1909@users.noreply.github.com>
2024-04-28 13:02:42 +0530

    Add files via upload

---
+++ mail.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer');
+const mailGun = require('nodemailer-mailgun-transport');
+
+
+
+const auth = {
+    auth: {
+        api_key: '9ac8cb56a715d751b7428df224b28ee8-19806d14-d65cde48',
+        domain: 'sandboxe6cc17c1269c45e1b70b2246ab336da3.mailgun.org',
+    }
+};
+
+const transporter = nodemailer.createTransport(mailGun(auth));
+
+
+const sendMail = (email, subject, text, cb) => {
+const mailOptions = {
+    from: email,
+    to: 'akshitaa390@gmail.com',
+    subject,
+    text
+};
+
+transporter.sendMail(mailOptions, function(err, data) {
+    if (err) {
+        cb(err, null);
+    } else {
+        cb(null, data);
+    }
+});
+}
+
+module.exports = sendMail;
+
